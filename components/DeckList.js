2a3d35f386a1b9a3c875b6818179ee207c4f718d
pietore <ungericw@tutanota.com>
2019-01-02 22:40:53 -0600

    api

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { submitDeck, submitCard, getDeck, getCard } from '../utils/helpers';
 
 export default class DeckList extends Component {
+	componentDidMount() {
+
+	}
   render() {
     return (
-		<View style={styles.container}>
-        <TextInput>Hey there!</TextInput>
+			<View style={styles.container}>
+        <TextInput>udacicards!</TextInput>
+        <TextInput>udacicards!</TextInput>
+        <TextInput>udacicards!</TextInput>
       </View>
     );
   }
