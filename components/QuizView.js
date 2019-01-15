f8d168bec8d451daed938d6d89b8be7d50df1ab7
pietore <ungericw@tutanota.com>
2019-01-14 23:10:18 -0600

    quiz

--- components/QuizView.js
+++ components/QuizView.js
@@ -4,41 +4,63 @@ import { FlatList } from 'react-native-gesture-handler';
 import { AsyncStorage } from 'react-native';
 import { getDecks } from '../utils/api';
 
-export const asyncStorageDeck = "flashcards:decks";
+const QuestionType = "QuestionType";
+const AnswerType = "AnswerType";
 
 export default class QuizView extends Component {
+	state = {
+		display: QuestionType
+	}
+	switchDisplay() {
+		if (this.state.display === QuestionType) {
+			this.setState({
+				display: AnswerType
+			})
+		} else {
+			this.setState({
+				display: QuestionType
+			})
+		}
+	}
+	answerCorrect() {
+
+		return;	
+	}
+	answerIncorrect() {
+		return;
+	}
 	render() {
-		// console.log(this.props.navigation.state.params.deck);
+		var entry = this.props.navigation.state.params.deck.item.questions[0];
 		return (
-			<View style={styles.contentContainer} >
-				<FlatList contentContainerStyle={styles.contentContainer} data={ this.props.navigation.state.params.deck.item.questions }
-					renderItem={entry => {
-						console.log( entry.item.question );
-						return (
-							<View>
-								<TouchableOpacity onPress={entry => this.onClick()}>
-									<View>
-										<Text style={{ color: 'black' }}>{entry.item.question}</Text>
-									</View>
-								</TouchableOpacity>
-								<TouchableOpacity onPress={entry => this.onClick()}>
-									<View>
-										<Text style={{ color: 'black' }}>Answer</Text>
-									</View>
-								</TouchableOpacity>
-								<TouchableOpacity onPress={entry => this.onClick()}>
-									<View>
-										<Text style={{ color: 'black' }}>Correct</Text>
-									</View>
-								</TouchableOpacity>
-								<TouchableOpacity onPress={entry => this.onClick()}>
-									<View>
-										<Text style={{ color: 'black' }}>Incorrect</Text>
-									</View>
-								</TouchableOpacity>
-							</View>
-						)
-					}} />
+			<View style={styles.contentContainer}>
+				<View>
+					<View>
+						{
+							(this.state.display === QuestionType) ?
+								<Text style={{ color: 'black' }}>{ entry.question }</Text>
+								:
+								<Text style={{ color: 'black' }}>{ entry.answer }</Text>
+						}
+					</View>
+					<TouchableOpacity onPress={entry => this.switchDisplay()}>
+						{
+							(this.state.display === QuestionType) ?
+								<Text style={{ color: 'black' }}>Answer</Text>
+								:
+								<Text style={{ color: 'black' }}>Question</Text>
+						}
+					</TouchableOpacity>
+					<TouchableOpacity onPress={entry => this.answerCorrect()}>
+						<View>
+							<Text style={{ color: 'black' }}>Correct</Text>
+						</View>
+					</TouchableOpacity>
+					<TouchableOpacity onPress={entry => this.answerIncorrect()}>
+						<View>
+							<Text style={{ color: 'black' }}>Incorrect</Text>
+						</View>
+					</TouchableOpacity>
+				</View>
 			</View>
 		);
 	}
